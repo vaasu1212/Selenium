ed649e8282954acc9a61ec6d3b08a3ac31ceedbc
Vasu Modiboyina <vasu.modiboyina@virginmoney.com>
2018-11-26 06:48:08 -0800

    adding all

---
+++ TestProgram.cs
@@ -0,0 +1,64 @@
+using NUnit.Framework;
+
+using NUnit;
+
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Firefox;
+
+using System;
+
+[TestFixture]
+
+public class TestProgram
+
+{
+
+private IWebDriver driver;
+
+[SetUp]
+
+public void SetupTest()
+
+{
+
+//FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@”/Users/rakeshprabhakaran/Project/c#/expOne/lib/”, “geckodriver”);
+FirefoxDriverService service = FirefoxDriverService.CreateDefaultService("F:\\ProjectFolder\\Drivers", "geckodriver.exe");
+
+//service.Port = 64444;
+
+driver = new FirefoxDriver(service);
+Console.WriteLine("Selenium Test1");
+driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+driver.Url ="https://code.visualstudio.com/";
+
+}
+
+[TearDown]
+
+public void TeardownTest()
+
+{
+
+driver.Quit();
+
+}
+
+[Test]
+
+public void testMethod()
+
+{
+
+String title = driver.Title;
+
+System.Console.WriteLine("title of site is : "+title);
+
+//IWebElement button =driver.FindElement(By.CssSelector('.link-button.dlink'));
+
+//Assert.AreEqual(“Download for Mac\nStable Build” , button.Text);
+
+}
+
+}
